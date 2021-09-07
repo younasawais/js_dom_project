c22c96511b90e781fa6181cb38fad0ff5c325730
Mohammad.Younas@coxautoinc.com <Mohammad.Younas@coxautoinc.com>
2021-09-06 21:24:53 -0400

    plus btn works - append content

--- script.js
+++ script.js
@@ -25,26 +25,42 @@ function confirmNames(){
 
 function setVehiclesAmount(val){
     val === '+' && (totalVehicles += 1) || (totalVehicles > 0 && (totalVehicles -= 1));
+    /**** Create all nodes ****/
+    let vehicleDiv = document.createElement('div'); // Add main vehicle div
+    let colorDivH2 = document.createElement('div'); 
+    let colorButtonsDiv = document.createElement('div');
+    let h2DivSizecar = document.createElement('div');
+    let sizeVehicleDiv = document.createElement('div');
+    let sizeVehicleDivLength = document.createElement('div');
+    let sizeVehicleDivInputLength = document.createElement('div');
+    let sizeVehicleDivInputWidth = document.createElement('div');
+    let sizeVehicleDivWidth = document.createElement('div');
+    let vehicleSizeDiv = document.createElement('div');
+    let vehicleSizeP = document.createElement('p');
+    let sizeVehicleDivWidthP = document.createElement('p');
+    let sizeVehicleDivLengthP = document.createElement('p');
+    let colorH2 = document.createElement('h2');
+    let h2Sizecar = document.createElement('h2');
+    
+    // Append new vehicle div to vehicles container
+    document.getElementsByClassName('vehicles')[0].appendChild(vehicleDiv);
+
+    // colorDivH2 
+    colorH2.innerHTML = `What color of vehicle would you prefer for vehicle ${totalVehicles}`;
+    colorDivH2.appendChild(colorH2);
+    vehicleDiv.appendChild(colorDivH2);
+    // colorButtonsDiv
+    colorButtonsDiv.setAttribute('class', 'colorButtons');
     const colorButtons = ['green', 'grey', 'yellow', 'brown'];
-    for (let i = 0; i < totalVehicles; i++) {
-        //console.log("🚀 ~ file: script.js ~ line 31 ~ setVehiclesAmount ~ totalVehicles", totalVehicles);
-        let h2Div = document.createElement('div');
-        console.log("🚀 ~ file: script.js ~ line 33 ~ setVehiclesAmount ~ h2Div", h2Div);
-        let h2 = document.createElement('h2');
-        h2.innerHTML = `What color of vehicle would you prefer for vehicle ${i+1}`;
-        h2Div.appendChild(h2);
-        console.log('h2Div', h2Div);
-        let colorButtonsDiv = null;
-        for (let i = 0; i < colorButtons.length; i++) {
-            let colorButton = document.createElement('button');
-            colorButton.innerHTML = colorButtons[i];
-            colorButtonsDiv = document.createElement('div').appendChild(colorButton);
-            colorButtonsDiv.setAttribute('class', 'colorButtons');
-        }
-        let vehicleDiv = document.createElement('div');
-        vehicleDiv.appendChild(h2Div);
-        vehicleDiv.appendChild(colorButtonsDiv);
-        console.log(vehicleDiv);
-        document.getElementsByClassName('vehicles')[0].appendChild(vehicleDiv);        
-    }
+    vehicleDiv.appendChild(colorButtonsDiv);
+    console.log("🚀 ~ file: script.js ~ line 54 ~ setVehiclesAmount ~ vehicleDiv", vehicleDiv);
+    for (let i = 0; i < colorButtons.length; i++) {
+    console.log("🚀 ~ file: script.js ~ line 55 ~ setVehiclesAmount ~ colorButtonsDiv", colorButtonsDiv);
+        let colorButton = document.createElement('button');
+        colorButton.innerHTML = colorButtons[i];
+        let colorButtonDiv = document.createElement('div');
+        colorButtonDiv.appendChild(colorButton);
+        colorButtonsDiv.appendChild(colorButtonDiv);
+    };     
+    
 }
